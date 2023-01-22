92277c80e58c579e0e0b717a24f1e2b7832e493d
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-01-22 12:28:49 +0900

    fix bugs

--- extract.py
+++ extract.py
@@ -81,4 +81,4 @@ class MPObject():
 
     def image2video(out_path):
         fps_r = self.fps/self.interval
-        command ='ffmpeg -y -r '+ str(fps_r) ' -i images/%6d.jpg -vcodec libx264 -pix_fmt yuv420p -loglevel error '+out_path
+        command='ffmpeg -y -r '+ str(fps_r) ' -i images/%6d.jpg -vcodec libx264 -pix_fmt yuv420p -loglevel error '+out_path

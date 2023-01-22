f97d9b0be207990df3e4a2ceeedb044622d1c54e
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-01-22 14:02:37 +0900

    fix bugs

--- extract.py
+++ extract.py
@@ -91,7 +91,7 @@ class MPObject():
         os.system(command)
 
     def show_landmark(self, part='LEFT', frame=0):
-        img = 'images/%6d.jpg' % (frame)
+        img = 'images/%06d.jpg' % (frame)
         print(img)
         cv2_imshow(img)
         if part == 'LEFT':

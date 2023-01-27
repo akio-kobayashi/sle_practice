8fb34aa00920da297da90ac40978a0251a4bdfb9
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-01-27 16:22:31 +0900

    add functions

--- extract.py
+++ extract.py
@@ -42,12 +42,18 @@ class MPObject():
 
     def apply_mp(self):
         reset_folder('images')
+        reset_folder('pose_images')
+        
         files = []
         for name in sorted(glob.glob('./frames/*.jpg')):
             files.append(name)
         images = {name: cv2.imread(name) for name in files}
 
         self.data=[]
+        img_width = images[0].shape[1]
+        img_height = images[1].shape[0]
+        background = np.zeros((img_height, img_width), np.uint8)
+
         for name, image in tqdm(images.items()):
             results = self.holistic.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
             csv=[]
@@ -147,12 +153,45 @@ class MPObject():
             save_name = 'images/'+os.path.basename(name)
             cv2.imwrite(save_name, annotated_image)
 
+            annotated_image = background.copy()
+            self.mp_drawing.draw_landmarks(
+                image = annotated_image,
+                landmark_list = results.left_hand_landmarks,
+                connections = self.mp_holistic.HAND_CONNECTIONS,
+                landmark_drawing_spec = self.drawing_dot_spec,
+                connection_drawing_spec = self.drawing_hand_spec
+            )
+            self.mp_drawing.draw_landmarks(
+                image = annotated_image,
+                landmark_list = results.right_hand_landmarks,
+                connections = self.mp_holistic.HAND_CONNECTIONS,
+                landmark_drawing_spec = self.drawing_dot_spec,
+                connection_drawing_spec = self.drawing_hand_spec
+            )
+            self.mp_drawing.draw_landmarks(
+                image = annotated_image,
+                landmark_list = results.face_landmarks,
+                connections = self.mp_holistic.FACEMESH_TESSELATION,
+                landmark_drawing_spec = self.drawing_face_spec,
+                connection_drawing_spec = self.drawing_face_spec
+            )
+            self.mp_drawing.draw_landmarks(
+                image = annotated_image,
+                landmark_list = results.pose_landmarks,
+                connections = self.mp_holistic.POSE_CONNECTIONS,
+                landmark_drawing_spec = self.drawing_pose_spec,
+                connection_drawing_spec = self.drawing_pose_spec
+            )
+
+            save_name = 'pose_images/'+os.path.basename(name)
+            cv2.imwrite(save_name, annotated_image)
+
     def image2video(self, out_path):
         fps_r = self.fps/self.interval
         command='ffmpeg -y -r {0} -i images/%6d.jpg -vcodec libx264 -pix_fmt yuv420p -loglevel error {1}'.format(str(fps_r), out_path)
-        #command='ffmpeg -y -r -i images/%6d.jpg -vcodec libx264 -pix_fmt yuv420p -loglevel error '
         os.system(command)
 
     def save_data(self, out_path):
+        # 最初から10フレームだけ記録する
         df = pd.DataFrame(self.data[:10], columns=self.label)
         df.to_csv(out_path)
